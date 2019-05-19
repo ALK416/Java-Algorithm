e41ed2e0c391624547c5e2b56090abffc6e6183d
Mryoungrealman <33858317+Mryoungrealman@users.noreply.github.com>
2019-05-19 22:06:20 +0800

    Add files via upload
    ’ GUIElenments.java‘是前端界面在前端界面中添加事件响应调用超市类中的处理方法；'Supermarket.java'为后端处理超市类，其中包含最主要的处理方法；'Main_test.java'类为主函数入口，这个类中仅有一个main主函数。

---
+++ Main_test.java
@@ -0,0 +1,8 @@
+package experiment03_Supermarket;
+
+public class Main_test {
+	public static void main(String[] args) {
+		Supermarket sup = new Supermarket();
+		LoginDialog l1 = new LoginDialog(sup);
+	}
+}
